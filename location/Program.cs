ea7d44f25f569a20eedd67bea8a3da007c530870
mattliney <M.LINEY-2020@hull.ac.uk>
2022-02-08 16:32:08 +0000

    done lab 1

--- location/Program.cs
+++ location/Program.cs
@@ -7,12 +7,43 @@ public class Whois
     static void Main(string[] args)
     {
         int c;
-        TcpClient client = new TcpClient();
-        client.Connect("whois.networksolutions.com", 43);
-        StreamWriter sw = new StreamWriter(client.GetStream());
-        StreamReader sr = new StreamReader(client.GetStream());
-        sw.WriteLine(args[0]);
-        sw.Flush();
-        Console.WriteLine(sr.ReadToEnd());
+        try
+        {
+            TcpClient client = new TcpClient();
+            client.Connect("whois.net.dcs.hull.ac.uk", 43);
+            StreamWriter writer = new StreamWriter(client.GetStream());
+            StreamReader reader = new StreamReader(client.GetStream());
+
+            if (args.Length == 1)
+            {
+                writer.WriteLine(args[0]);
+                writer.Flush();
+                Console.WriteLine(args[0] + " is " + reader.ReadToEnd());
+            }
+            else if(args.Length == 2)
+            {
+                writer.WriteLine(args[0] + " " + args[1]);
+                writer.Flush();
+
+                string error = reader.ReadToEnd();
+
+                if(error == "OK\r\n")
+                {
+                    Console.WriteLine(args[0] + " location changed to be " + args[1]);
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: Something went wrong");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong");
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Something went wrong");
+        }
     }
 }
