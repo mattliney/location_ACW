7c37941ec50904061f5086f62af8078870c25e0f
mattliney <M.LINEY-2020@hull.ac.uk>
2022-02-08 15:03:53 +0000

    Add project files.

---
+++ location/Program.cs
@@ -0,0 +1,18 @@
+﻿//Demonstrate Sockets
+using System;
+using System.Net.Sockets;
+using System.IO;
+public class Whois
+{
+    static void Main(string[] args)
+    {
+        int c;
+        TcpClient client = new TcpClient();
+        client.Connect("whois.networksolutions.com", 43);
+        StreamWriter sw = new StreamWriter(client.GetStream());
+        StreamReader sr = new StreamReader(client.GetStream());
+        sw.WriteLine(args[0]);
+        sw.Flush();
+        Console.WriteLine(sr.ReadToEnd());
+    }
+}
